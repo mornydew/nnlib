cadfa48fd8011a1d1ae45ea0217ef28e906a3310
Luke Godfrey <thelukester92@me.com>
2017-11-07 14:17:20 -0600

    Partial implementation.

--- include/nnlib/math/math_base.hpp
+++ include/nnlib/math/math_base.hpp
@@ -2,6 +2,7 @@
 #define MATH_MATH_BASE_HPP
 
 #include "../core/error.hpp"
+#include "../core/tensor.hpp"
 
 namespace nnlib
 {
@@ -14,14 +15,28 @@ class MathBase
 public:
 	// MARK: Vector/scalar operations
 	
-	/// x[i] = alpha, 0 <= i < n
+	/// x[i] = alpha
+	static void vFill(Tensor<T> &x, T alpha)
+	{
+		NNAssertEquals(x.dims(), 1, "Expected a vector!");
+		vFill(x.ptr(), x.size(), x.stride(0), alpha);
+	}
+	
+	/// x[i] = alpha
 	static void vFill(T *x, size_t n, size_t s, T alpha)
 	{
 		for(size_t i = 0; i < n; ++i)
 			x[i * s] = alpha;
 	}
 	
-	/// x[i] *= alpha, 0 <= i < n
+	/// x[i] *= alpha
+	static void vScale(Tensor<T> &x, T alpha)
+	{
+		NNAssertEquals(x.dims(), 1, "Expected a vector!");
+		vScale(x.ptr(), x.size(), x.stride(0), alpha);
+	}
+	
+	/// x[i] *= alpha
 	static void vScale(T *x, size_t n, size_t s, T alpha)
 	{
 		for(size_t i = 0; i < n; ++i)
@@ -30,7 +45,14 @@ public:
 	
 	// MARK: Matrix/scalar operations
 	
-	/// A[i][j] = alpha, 0 <= i < ra, 0 <= j < ca
+	/// A[i][j] = alpha
+	static void mFill(Tensor<T> &A, T alpha)
+	{
+		NNAssertEquals(A.dims(), 2, "Expected a matrix!");
+		mFill(A.ptr(), A.size(0), A.size(1), A.stride(0), alpha);
+	}
+	
+	/// A[i][j] = alpha
 	static void mFill(T *A, size_t r, size_t c, size_t ld, T alpha)
 	{
 		for(size_t i = 0; i < r; ++i)
@@ -38,7 +60,14 @@ public:
 				A[i * ld + j] = alpha;
 	}
 	
-	/// A[i][j] *= alpha, 0 <= i < ra, 0 <= j < ca
+	/// A[i][j] *= alpha
+	static void mScale(Tensor<T> &A, T alpha)
+	{
+		NNAssertEquals(A.dims(), 2, "Expected a matrix!");
+		mScale(A.ptr(), A.size(0), A.size(1), A.stride(0), alpha);
+	}
+	
+	/// A[i][j] *= alpha
 	static void mScale(T *A, size_t r, size_t c, size_t ld, T alpha)
 	{
 		for(size_t i = 0; i < r; ++i)
@@ -49,11 +78,16 @@ public:
 	// MARK: Vector/Vector operations
 	
 	/// y = alpha * x + beta * y
-	static void vAdd_v(
-		const T *x, size_t n, size_t sx,
-		T *y, size_t sy,
-		T alpha = 1, T beta = 1
-	)
+	static void vAdd_v(const Tensor<T> &x, Tensor<T> &y, T alpha = 1, T beta = 1)
+	{
+		NNAssertEquals(x.dims(), 1, "Expected a vector!");
+		NNAssertEquals(y.dims(), 1, "Expected a vector!");
+		NNAssertEquals(x.size(), y.size(), "Incompatible operands!");
+		vAdd_v(x.ptr(), x.size(), x.stride(0), y.ptr(), y.stride(0), alpha, beta);
+	}
+	
+	/// y = alpha * x + beta * y
+	static void vAdd_v(const T *x, size_t n, size_t sx, T *y, size_t sy, T alpha = 1, T beta = 1)
 	{
 		for(size_t i = 0; i < n; ++i)
 			y[i * sy] = alpha * x[i * sx] + beta * y[i * sy];
@@ -62,12 +96,18 @@ public:
 	// MARK: Matrix/Vector operations
 	
 	/// A = alpha * x <*> y + beta * A, <*> = outer product
-	static void mAdd_vv(
-		const T *x, size_t r, size_t sx,
-		const T *y, size_t c, size_t sy,
-		T *A, size_t lda,
-		T alpha = 1, T beta = 1
-	)
+	static void mAdd_vv(const Tensor<T> &x, const Tensor<T> &y, Tensor<T> &A, T alpha = 1, T beta = 1)
+	{
+		NNAssertEquals(x.dims(), 1, "Expected a vector!");
+		NNAssertEquals(y.dims(), 1, "Expected a vector!");
+		NNAssertEquals(A.dims(), 2, "Expected a matrix!");
+		NNAssertEquals(A.size(0), x.size(), "Incompatible operands!");
+		NNAssertEquals(A.size(1), y.size(), "Incompatible operands!");
+		mAdd_vv(x.ptr(), x.size(), x.stride(0), y.ptr(), y.size(), y.stride(0), A.ptr(), A.stride(0), alpha, beta);
+	}
+	
+	/// A = alpha * x <*> y + beta * A, <*> = outer product
+	static void mAdd_vv(const T *x, size_t r, size_t sx, const T *y, size_t c, size_t sy, T *A, size_t lda, T alpha = 1, T beta = 1)
 	{
 		for(size_t i = 0; i < r; ++i)
 			for(size_t j = 0; j < c; ++j)
@@ -75,12 +115,19 @@ public:
 	}
 	
 	/// y = alpha * A * x^T + beta * y
-	static void vAdd_mv(
-		const T *A, size_t ra, size_t ca, size_t lda,
-		const T *x, size_t sx,
-		T *y, size_t sy,
-		T alpha = 1, T beta = 1
-	)
+	static void vAdd_mv(const Tensor<T> &A, const Tensor<T> &x, Tensor<T> &y, T alpha = 1, T beta = 1)
+	{
+		NNAssertEquals(A.dims(), 2, "Expected a matrix!");
+		NNAssertEquals(x.dims(), 1, "Expected a vector!");
+		NNAssertEquals(y.dims(), 1, "Expected a vector!");
+		NNAssertEquals(A.size(1), x.size(0), "Incompatible operands!");
+		NNAssertEquals(A.size(0), y.size(0), "Incompatible operands!");
+		NNAssertEquals(A.stride(1), 1, "Expected a contiguous matrix!");
+		vAdd_mv(A.ptr(), A.size(0), A.size(1), A.stride(0), x.ptr(), x.stride(0), y.ptr(), y.stride(0), alpha, beta);
+	}
+	
+	/// y = alpha * A * x^T + beta * y
+	static void vAdd_mv(const T *A, size_t ra, size_t ca, size_t lda, const T *x, size_t sx, T *y, size_t sy, T alpha = 1, T beta = 1)
 	{
 		for(size_t i = 0; i < ra; ++i)
 		{
@@ -92,12 +139,19 @@ public:
 	}
 	
 	/// y = alpha * A^T * x^T + beta * y
-	static void vAdd_mtv(
-		const T *A, size_t ra, size_t ca, size_t lda,
-		const T *x, size_t sx,
-		T *y, size_t sy,
-		T alpha = 1, T beta = 1
-	)
+	static void vAdd_mtv(const Tensor<T> &A, const Tensor<T> &x, Tensor<T> &y, T alpha = 1, T beta = 1)
+	{
+		NNAssertEquals(A.dims(), 2, "Expected a matrix!");
+		NNAssertEquals(x.dims(), 1, "Expected a vector!");
+		NNAssertEquals(y.dims(), 1, "Expected a vector!");
+		NNAssertEquals(A.size(0), x.size(0), "Incompatible operands!");
+		NNAssertEquals(A.size(1), y.size(0), "Incompatible operands!");
+		NNAssertEquals(A.stride(1), 1, "Expected a contiguous matrix!");
+		vAdd_mtv(A.ptr(), A.size(0), A.size(1), A.stride(0), x.ptr(), x.stride(0), y.ptr(), y.stride(0), alpha, beta);
+	}
+	
+	/// y = alpha * A^T * x^T + beta * y
+	static void vAdd_mtv(const T *A, size_t ra, size_t ca, size_t lda, const T *x, size_t sx, T *y, size_t sy, T alpha = 1, T beta = 1)
 	{
 		for(size_t i = 0; i < ca; ++i)
 		{
@@ -111,11 +165,16 @@ public:
 	// MARK: Matrix/Matrix operations
 	
 	/// B = alpha * A + beta * B
-	static void mAdd_m(
-		const T *A, size_t r, size_t c, size_t lda,
-		T *B, size_t ldb,
-		T alpha = 1, T beta = 1
-	)
+	static void mAdd_m(const Tensor<T> &A, Tensor<T> &B, T alpha = 1, T beta = 1)
+	{
+		NNAssertEquals(A.dims(), 2, "Expected a matrix!");
+		NNAssertEquals(B.dims(), 2, "Expected a matrix!");
+		NNAssertEquals(A.shape(), B.shape(), "Incompatible operands!");
+		mAdd_m(A.ptr(), A.size(0), A.size(1), A.stride(0), ptr(), stride(0), alpha, beta);
+	}
+	
+	/// B = alpha * A + beta * B
+	static void mAdd_m(const T *A, size_t r, size_t c, size_t lda, T *B, size_t ldb, T alpha = 1, T beta = 1)
 	{
 		for(size_t i = 0; i < r; ++i)
 			for(size_t j = 0; j < c; ++j)
@@ -123,11 +182,17 @@ public:
 	}
 	
 	/// B = alpha * A^T + beta * B
-	static void mAdd_mt(
-		const T *A, size_t r, size_t c, size_t lda,
-		T *B, size_t ldb,
-		T alpha = 1, T beta = 1
-	)
+	static void mAdd_mt(const Tensor<T> &A, Tensor<T> &B, T alpha = 1, T beta = 1)
+	{
+		NNAssertEquals(A.dims(), 2, "Expected a matrix!");
+		NNAssertEquals(B.dims(), 2, "Expected a matrix!");
+		NNAssertEquals(A.size(0), B.size(1), "Incompatible operands!");
+		NNAssertEquals(A.size(1), B.size(0), "Incompatible operands!");
+		mAdd_mt(A.ptr(), A.size(0), A.size(1), A.stride(0), ptr(), stride(0), alpha, beta);
+	}
+	
+	/// B = alpha * A^T + beta * B
+	static void mAdd_mt(const T *A, size_t r, size_t c, size_t lda, T *B, size_t ldb, T alpha = 1, T beta = 1)
 	{
 		for(size_t i = 0; i < r; ++i)
 			for(size_t j = 0; j < c; ++j)
@@ -135,13 +200,22 @@ public:
 	}
 	
 	/// C = alpha * A * B + beta * C
-	static void mAdd_mm(
-		size_t M, size_t N, size_t K,
-		const T *A, size_t lda,
-		const T *B, size_t ldb,
-		T *C, size_t ldc,
-		T alpha = 1, T beta = 1
-	)
+	static void mAdd_mm(const Tensor<T> &A, const Tensor<T> &B, Tensor<T> &C, T alpha = 1, T beta = 1)
+	{
+		NNAssertEquals(A.dims(), 2, "Expected a matrix!");
+		NNAssertEquals(B.dims(), 2, "Expected a matrix!");
+		NNAssertEquals(C.dims(), 2, "Expected a matrix!");
+		NNAssertEquals(A.stride(1), 1, "Expected a contiguous matrix!");
+		NNAssertEquals(B.stride(1), 1, "Expected a contiguous matrix!");
+		NNAssertEquals(C.stride(1), 1, "Expected a contiguous matrix!");
+		NNAssertEquals(A.size(0), C.size(0), "Incompatible operands!");
+		NNAssertEquals(B.size(1), C.size(1), "Incompatible operands!");
+		NNAssertEquals(A.size(1), B.size(0), "Incompatible operands!");
+		mAdd_mm(A.size(0), B.size(1), A.size(1), A.ptr(), A.stride(0), B.ptr(), B.stride(0), C.ptr(), C.stride(0), alpha, beta);
+	}
+	
+	/// C = alpha * A * B + beta * C
+	static void mAdd_mm(size_t M, size_t N, size_t K, const T *A, size_t lda, const T *B, size_t ldb, T *C, size_t ldc, T alpha = 1, T beta = 1)
 	{
 		for(size_t i = 0; i < M; ++i)
 		{
@@ -158,13 +232,22 @@ public:
 	}
 	
 	/// C = alpha * A^T * B + beta * C
-	static void mAdd_mtm(
-		size_t M, size_t N, size_t K,
-		const T *A, size_t lda,
-		const T *B, size_t ldb,
-		T *C, size_t ldc,
-		T alpha = 1, T beta = 1
-	)
+	static void mAdd_mtm(const Tensor<T> &A, const Tensor<T> &B, Tensor<T> &C, T alpha = 1, T beta = 1)
+	{
+		NNAssertEquals(A.dims(), 2, "Expected a matrix!");
+		NNAssertEquals(B.dims(), 2, "Expected a matrix!");
+		NNAssertEquals(C.dims(), 2, "Expected a matrix!");
+		NNAssertEquals(A.stride(1), 1, "Expected a contiguous matrix!");
+		NNAssertEquals(B.stride(1), 1, "Expected a contiguous matrix!");
+		NNAssertEquals(C.stride(1), 1, "Expected a contiguous matrix!");
+		NNAssertEquals(A.size(1), C.size(0), "Incompatible operands!");
+		NNAssertEquals(B.size(1), C.size(1), "Incompatible operands!");
+		NNAssertEquals(A.size(0), B.size(0), "Incompatible operands!");
+		mAdd_mtm(A.size(1), B.size(1), A.size(0), A.ptr(), A.stride(0), B.ptr(), B.stride(0), C.ptr(), C.stride(0), alpha, beta);
+	}
+	
+	/// C = alpha * A^T * B + beta * C
+	static void mAdd_mtm(size_t M, size_t N, size_t K, const T *A, size_t lda, const T *B, size_t ldb, T *C, size_t ldc, T alpha = 1, T beta = 1)
 	{
 		for(size_t i = 0; i < M; ++i)
 		{
@@ -181,13 +264,22 @@ public:
 	}
 	
 	/// C = alpha * A * B^T + beta * C
-	static void mAdd_mmt(
-		size_t M, size_t N, size_t K,
-		const T *A, size_t lda,
-		const T *B, size_t ldb,
-		T *C, size_t ldc,
-		T alpha = 1, T beta = 1
-	)
+	static void mAdd_mmt(const Tensor<T> &A, const Tensor<T> &B, Tensor<T> &C, T alpha = 1, T beta = 1)
+	{
+		NNAssertEquals(A.dims(), 2, "Expected a matrix!");
+		NNAssertEquals(B.dims(), 2, "Expected a matrix!");
+		NNAssertEquals(C.dims(), 2, "Expected a matrix!");
+		NNAssertEquals(A.stride(1), 1, "Expected a contiguous matrix!");
+		NNAssertEquals(B.stride(1), 1, "Expected a contiguous matrix!");
+		NNAssertEquals(C.stride(1), 1, "Expected a contiguous matrix!");
+		NNAssertEquals(A.size(0), C.size(0), "Incompatible operands!");
+		NNAssertEquals(B.size(0), C.size(1), "Incompatible operands!");
+		NNAssertEquals(A.size(1), B.size(1), "Incompatible operands!");
+		mAdd_mmt(A.size(0), B.size(0), A.size(1), A.ptr(), A.stride(0), B.ptr(), B.stride(0), C.ptr(), C.stride(0), alpha, beta);
+	}
+	
+	/// C = alpha * A * B^T + beta * C
+	static void mAdd_mmt(size_t M, size_t N, size_t K, const T *A, size_t lda, const T *B, size_t ldb, T *C, size_t ldc, T alpha = 1, T beta = 1)
 	{
 		for(size_t i = 0; i < M; ++i)
 		{
